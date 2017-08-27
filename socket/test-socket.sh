eb95cfcb3cca06aad369f3c4dc7b6ac1a5857b9b
Susant Sahani <susant@redhat.com>
2017-08-27 20:39:37 +0530

    Add socket activation to test

---
+++ socket/test-socket.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+set -x
+set -e
+set -o pipefail
+
+echo "Starting test socket ..."
+
+systemctl stop test.socket
+systemctl stop test-protocol.socket
+
+systemctl start test.socket
+
+# ListenStream=
+[[ "$(netstat -antp | grep 9999 | awk '{print $NF}')" == "1/systemd" ]]
+
+# ListenDatagram=
+[[ "$(netstat -anup | grep 9999 | awk '{print $NF}')" == "1/systemd" ]]
+
+# ListenSequentialPacket= Unix domain
+[[ "$(netstat -lxp | grep "/var/run/test.socket" | awk '{print $6}')" == "SEQPACKET" ]]
+[[ "$(netstat -lxp | grep "/var/run/test.socket" | awk '{print $9}')" == "1/systemd" ]]
+
+[[ -p /var/run/test-fifo ]]
+
+systemctl start test-protocol.socket
+
+# ListenStream=sctp
+[[ "$(netstat -antp | grep 9998 | awk '{print $NF}')" == "1/systemd" ]]
+
+# ListenDatagram=udplite
+[[ "$(lsof -p 1 | grep UDPLITE | awk '{print $NF}')" == "localhost:9998" ]]
+
+
+touch /tmp/testok
+
+systemctl stop test.socket
+systemctl stop test-protocol.socket
